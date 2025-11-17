fb3957e04cf5477387b52255c0b72cb7d2019b63
Aman <amankumar65@deloitte.com>
2025-11-17 19:49:30 +0530

    middleware removed.

--- src/container.ts
+++ src/container.ts
@@ -1,15 +1,15 @@
 import "reflect-metadata"
 import { Container } from "inversify"
 import { TYPES } from "./types.js"
-import { NominatimService } from "./services/nominatimService.js"
-import { CityService } from "./services/cityService.js"
 import { CityController } from "./controllers/cityController.js"
+import { CityService } from "./services/cityService.js"
+import { NominatimService } from "./services/nominatimService.js"
 
 const container = new Container()
 
 // Creates the DI (Dependency Injection) container and binds interfaces/identifiers to concrete classes.
 container.bind(TYPES.CityService).to(CityService).inSingletonScope()
 container.bind(TYPES.NominatimService).to(NominatimService).inSingletonScope()
-container.bind(TYPES.CityController).toSelf().inSingletonScope()
+container.bind(TYPES.CityController).to(CityController).inSingletonScope()
 
 export {container};
